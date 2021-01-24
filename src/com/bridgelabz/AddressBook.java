bdcbb7155fa1a038c1060f91358bde665afe26c9
ajay812998 <ajay812998@gmail.com>
2021-01-24 22:45:28 +0530

    Added : Adding New Contact

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -3,6 +3,9 @@ package com.bridgelabz;
 public class AddressBook {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Problem");
+		AddressMethods address=new AddressMethods();
+		address.Add();
+		address.Show();
 	}
 }
 
